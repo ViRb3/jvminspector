13a46e76c8355d26e22ac2f13c4e8c95d76604a0
Victor <virb3e@gmail.com>
2020-01-04 23:55:03 +0200

    Dynamically load JDK dependencies

--- src/main/java/jvminspector/Main.java
+++ src/main/java/jvminspector/Main.java
@@ -4,14 +4,43 @@ package jvminspector;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 public class Main {
-	
-	public static final Logger logger = Logger.getLogger("");
-	
-    public static void main(String[] args)
-    {
+
+    public static final Logger logger = Logger.getLogger("");
+
+    private static String getJDKLibPath(File javaHome, String libName) {
+        return javaHome.getName().equalsIgnoreCase("jre") ? "../lib/" + libName : "lib/" + libName;
+    }
+
+    private void loadDependencies() throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        File javaHome = new File(System.getProperty("java.home"));
+        String toolsPath = getJDKLibPath(javaHome, "tools.jar");
+        String saJdiPath = getJDKLibPath(javaHome, "sa-jdi.jar");
+
+        ArrayList<URL> urls = new ArrayList<URL>(Arrays.asList(((URLClassLoader) getClass().getClassLoader()).getURLs()));
+        urls.add(new File(javaHome, toolsPath).getCanonicalFile().toURI().toURL());
+        urls.add(new File(javaHome, saJdiPath).getCanonicalFile().toURI().toURL());
+
+        URLClassLoader loader = new URLClassLoader(urls.toArray(new URL[0]), null);
+        Class<?> utilityClass = loader.loadClass("jvminspector.Main");
+
+        utilityClass.getMethod("showWindow").invoke(null);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().loadDependencies();
+    }
+
+    public static void showWindow() {
         EventQueue.invokeLater(
                 () ->
                 {
